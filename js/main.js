f395ce8fff21a96cea4dfe069a92b7657ca24e05
lhguerra <lhguerra@gmail.com>
2014-02-03 17:22:29 -0200

    politicamp files added

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// Animation for 'saiba mais' transition
+jQuery(document).ready(function() {
+  $(".scroll").click(function(event){   
+    event.preventDefault();
+    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+  });
+});
+
+// Script for sent form message
+jQuery(document).ready(function() {
+  $("#professional button").click(function () {
+    $("#professional h3").css('display' , 'block');
+    $("#professional form").css('display' , 'none');
+  });
+});
